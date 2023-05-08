2fe3bcc0995dc494bc4294e81ed39a600dd0f0c0
Celal Şahin Altınışık <celalsahinaltinisik@gmail.com>
2023-05-08 17:17:17 +0300

    [feat] product storage relation pivot table created

---
+++ app/database/migrations/2023_5_08_000003_create_product_storage_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_storage', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBiginteger('product_id')->unsigned();
+            $table->unsignedBiginteger('storage_id')->unsigned();
+
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('storage_id')->references('id')->on('storages');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_storage');
+    }
+};
