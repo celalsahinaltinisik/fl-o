2b16cf82d0ffeda29722b92d091d6242a19fff9e
Celal Şahin Altınışık <celalsahinaltinisik@gmail.com>
2023-05-09 11:45:34 +0300

    [feat] db select

--- app/app/Repositories/Interfaces/Products/ProductRepositoryInterface.php
+++ app/app/Repositories/Interfaces/Products/ProductRepositoryInterface.php
@@ -3,7 +3,16 @@
 namespace App\Repositories\Interfaces\Products;
 
 use App\Repositories\Interfaces\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 interface ProductRepositoryInterface extends BaseRepositoryInterface
 {
+    /**
+     * Get whereIn models.
+     *
+     * @param array $columns
+     * @param array $values
+     * @return Collection
+     */
+    public function whereIn(array $columns = ['*'], array $values = []): Collection;
 }
--- app/app/Repositories/Services/Products/ProductRepository.php
+++ app/app/Repositories/Services/Products/ProductRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories\Services\Products;
 use App\Models\Product;
 use App\Repositories\Interfaces\Products\ProductRepositoryInterface;
 use App\Repositories\Services\BaseRepository;
+use Illuminate\Database\Eloquent\Collection;
 
 class ProductRepository extends BaseRepository implements ProductRepositoryInterface
 {
@@ -16,5 +17,15 @@ class ProductRepository extends BaseRepository implements ProductRepositoryInter
     public function __construct(Product $model)
     {
         parent::__construct($model);
+    }
+
+    /**
+     * @param array $columns
+     * @param array $values
+     * @return Collection
+     */
+    public function whereIn(array $columns = ['*'], array $values = []): Collection
+    {
+        return $this->newQuery()->whereIn('id', $values)->get($columns);
     }
 }
